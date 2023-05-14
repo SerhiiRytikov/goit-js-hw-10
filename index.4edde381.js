b33a91442df949966e6f35eb221efdc0b87b5115
SerhiiRytikov <111732658+SerhiiRytikov@users.noreply.github.com>
2023-05-14 17:32:14 +0000

    Deploying to gh-pages from @ SerhiiRytikov/goit-js-hw-10@6c866aaef4c68ed267b6d8f47ffb1aae88a610af 🚀

--- index.4edde381.js
+++
@@ -1,2 +0,0 @@
-console.log(fetch(`https://restcountries.com/v3.1/name/${"Ukraine"}?fields=name,capital,population,flags,languages`).then((e=>{if(e.ok)return e.json();throw new Error(e.status)})));
-//# sourceMappingURL=index.4edde381.js.map
